8802f4507f0ae23a4a146b087ee546d156021769
Kolby Sisk <kolby.sisk@gmail.com>
2025-05-15 22:21:36 -0500

    setup caching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    useCache: true,
+  },
+  images: {
+    remotePatterns: [new URL("https://picsum.photos/**")],
+  },
 };
 
 export default nextConfig;
